7c542e9ceda553c3d1a197e4cf4837c0bda93aef
marcus-j <18111840+marcus-j@users.noreply.github.com>
2019-03-10 22:09:05 +0100

    cleans up ProductController

--- src/main/java/de/marcusjanke/casestudies/productupdater/controllers/ProductController.java
+++ src/main/java/de/marcusjanke/casestudies/productupdater/controllers/ProductController.java
@@ -1,25 +1,22 @@
 package de.marcusjanke.casestudies.productupdater.controllers;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import de.marcusjanke.casestudies.productupdater.domain.RawProduct;
 import de.marcusjanke.casestudies.productupdater.domain.StockedProduct;
 import de.marcusjanke.casestudies.productupdater.repositories.StockedProductRepository;
+
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.HttpStatus.*;
 
 /**
  * Product resource
@@ -40,19 +37,15 @@ public class ProductController {
 	 * 
 	 * @return List of all products with id, name and description
 	 */
-	@RequestMapping(value = "/products", method = RequestMethod.GET)
+	@GetMapping(value = "/products")
 	public @ResponseBody ResponseEntity<List<RawProduct>> getProducts() {
 		try {
 			List<RawProduct> products = StreamSupport.stream(stockedProductRepository.findAll().spliterator(), false)
-					.map(RawProduct::new).collect(Collectors.toList());
-			if (products.isEmpty()) {
-				return new ResponseEntity<List<RawProduct>>(Collections.emptyList(), HttpStatus.NOT_FOUND);
-			} else {
-				return new ResponseEntity<List<RawProduct>>(products, HttpStatus.OK);
-			}
+					.map(RawProduct::new).collect(toList());
+			return new ResponseEntity<>(products, products.isEmpty() ? NOT_FOUND : OK);
 		} catch (Exception e) {
 			logger.error("Could not handle request", e);
-			return new ResponseEntity<List<RawProduct>>(HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
 		}
 	}
 
@@ -61,21 +54,21 @@ public class ProductController {
 	 * available using e.g
 	 * http://localhost:8080/products/43b105a0-b5da-401b-a91d-32237ae418e4
 	 * 
-	 * @param id
+	 * @param id product id
 	 * @return one product with all available information including current stock
 	 */
-	@RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/products/{id}")
 	public @ResponseBody ResponseEntity<StockedProduct> getProduct(@PathVariable("id") String id) {
 		try {
 			StockedProduct product = stockedProductRepository.findOne(id);
 			if (Objects.nonNull(product)) {
-				return new ResponseEntity<StockedProduct>(product, HttpStatus.OK);
+				return new ResponseEntity<>(product, OK);
 			} else {
-				return new ResponseEntity<StockedProduct>(HttpStatus.NOT_FOUND);
+				return new ResponseEntity<>(NOT_FOUND);
 			}
 		} catch (Exception e) {
 			logger.error("Could not handle request", e);
-			return new ResponseEntity<StockedProduct>(HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
 		}
 	}
 }
